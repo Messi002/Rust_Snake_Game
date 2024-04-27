b3be7084274f8224bd0c275a0aa0b97d9bf10cf1
Messi002 <rostandmessi2@gmail.com>
2024-04-27 14:30:00 +0100

    updated main.rs for window draw 2d

--- src/snake.rs
+++ src/snake.rs
@@ -127,4 +127,17 @@ impl Snake {
     }
 
     pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
-       
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return false;
+    }
+}
