4f5a59272ec643112aa6a2c533010135d3807d23
Messi002 <rostandmessi2@gmail.com>
2024-05-24 17:36:39 +0100

    deleting complete

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 extern crate piston_window;
 extern crate rand;
 
-mod draw;
-mod snake;
-mod game;
 
-use piston_window::*;
-use piston_window::types::Color;
-
-use crate::game::Game;
-use crate::draw::to_coord_u32;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
@@ -38,4 +30,5 @@ fn main() {
             game.update(arg.dt);
         });
     }
-}
+} 
+
